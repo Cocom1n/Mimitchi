4fedf0766b8ab8aa0c91ebdc5402e814432bfef8
Joaset <jtf1wft@gmail.com>
2024-09-23 00:33:02 -0300

    mejoras
    JT

--- Assets/Scripts/Happiness.cs
+++ Assets/Scripts/Happiness.cs
@@ -7,6 +7,10 @@ public class Happiness : MonoBehaviour
 {
     [SerializeField] private Image barraFelicidad;
     [SerializeField] private float cantFelicidad;
+    [SerializeField] private GameObject triste;
+    [SerializeField] private GameObject normal;
+    [SerializeField] private GameObject feliz;
+    [SerializeField] private GameObject contento;
     void Start()
     {
         cantFelicidad = GetComponent<Feeding>().GetCantAlimento() + GetComponent<Cleaning>().GetCantAseo() + GetComponent<Sleeping>().GetCantSueño();
@@ -16,10 +20,45 @@ public class Happiness : MonoBehaviour
     {
         barraFelicidad.fillAmount = cantFelicidad / 300;
         ActualizarCantFelicidad();
+        ActualizarFelicidad();
     }
 
     public void ActualizarCantFelicidad()
     {
         cantFelicidad = GetComponent<Feeding>().GetCantAlimento() + GetComponent<Cleaning>().GetCantAseo() + GetComponent<Sleeping>().GetCantSueño();
+    }
+
+    public void ActualizarFelicidad()
+    {
+        float calcularFelicidad;
+        calcularFelicidad = (cantFelicidad * 100) / 300;
+        if (calcularFelicidad >= 75)
+        {
+            contento.SetActive(true);
+            feliz.SetActive(false);
+            normal.SetActive(false);
+            triste.SetActive(false);
+        }
+        else if (calcularFelicidad >= 50 && calcularFelicidad < 75)
+        {
+            contento.SetActive(false);
+            feliz.SetActive(true);
+            normal.SetActive(false);
+            triste.SetActive(false);
+        }
+        else if (calcularFelicidad >= 25 && calcularFelicidad < 50)
+        {
+            contento.SetActive(false);
+            feliz.SetActive(false);
+            normal.SetActive(true);
+            triste.SetActive(false);
+        }
+        else if (calcularFelicidad >= 0 && calcularFelicidad < 25)
+        {
+            contento.SetActive(false);
+            feliz.SetActive(false);
+            normal.SetActive(false);
+            triste.SetActive(true);
+        }
     }
 }
