b330616ddf52837cf7dd35c78bab16326d37e747
carvlc <cnsvilca@gmail.com>
2024-09-23 23:44:06 -0300

    feat: cambio de fondo con btn

---
+++ Assets/Scripts/ChangeRoom.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRoom : MonoBehaviour
+{
+    public List<GameObject> fondos = new List<GameObject>();
+    public int indice;
+
+    void Start() {
+        ActualizarFondo();
+    }
+    public void NextRoom(){
+        fondos[indice].SetActive(false);
+        indice = (indice + 1) % fondos.Count;
+        ActualizarFondo();
+       
+    }
+
+    public void PreviousRoom(){
+        fondos[indice].SetActive(false);
+        indice = (indice - 1 + fondos.Count) % fondos.Count;
+        ActualizarFondo();
+       
+    }
+
+    public void ActualizarFondo(){
+        fondos[indice].SetActive(true);
+    }
+}
