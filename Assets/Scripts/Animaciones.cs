25523be24c7cbf4b6b7e2940d2fd033633fccf8b
Carrasco <136844452+Cocom1n@users.noreply.github.com>
2024-09-28 01:47:28 -0300

    animaciones secundarias
    asdad

--- Assets/Scripts/Animaciones.cs
+++ Assets/Scripts/Animaciones.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Animaciones : MonoBehaviour
 {
     public Animator animator;
+    private float num;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,23 +13,51 @@ public class Animaciones : MonoBehaviour
         animator.SetBool("Dormir", false);
         animator.SetBool("Bañar", false);
         animator.SetBool("Jugar", false);
+        animator.SetBool("Alimentar2", false);
+        animator.SetBool("Bañar2", false);
+        animator.SetBool("Jugar2", false);
     }
 
     public void Animar(float aux)
     {
+        num=Random. Range(1f,3f);
+        Debug.Log(num);
         switch (aux)
         {
             case 1:
                 animator.SetBool("Alimentar", false);
                 animator.SetBool("Dormir", false);
                 animator.SetBool("Jugar", false);
-                animator.SetBool("Bañar", true);
+                animator.SetBool("Alimentar2", false);
+                animator.SetBool("Jugar2", false);
+                if(num<=2f)
+                {
+                    animator.SetBool("Bañar", true);
+                    animator.SetBool("Bañar2", false);
+                }
+                if(num>2f)
+                {
+                    animator.SetBool("Bañar", false);
+                    animator.SetBool("Bañar2", true);
+                }
+                
                 break;
             case 2:
-                animator.SetBool("Alimentar", true);
                 animator.SetBool("Dormir", false);
                 animator.SetBool("Jugar", false);
+                animator.SetBool("Jugar2", false);
                 animator.SetBool("Bañar", false);
+                animator.SetBool("Bañar2", false);
+                if(num<=2f)
+                {
+                    animator.SetBool("Alimentar", true);
+                    animator.SetBool("Alimentar2", false);
+                }
+                if(num>2f)
+                {
+                    animator.SetBool("Alimentar", false);
+                    animator.SetBool("Alimentar2", true);
+                }
                 break;
             case 3:
                 animator.SetBool("Alimentar", false);
@@ -39,8 +68,19 @@ public class Animaciones : MonoBehaviour
             case 4:
                 animator.SetBool("Alimentar", false);
                 animator.SetBool("Dormir", false);
-                animator.SetBool("Jugar", true);
                 animator.SetBool("Bañar", false);
+                animator.SetBool("Bañar2", false);
+                animator.SetBool("Alimentar2", false);
+                if(num<=2f)
+                {
+                    animator.SetBool("Jugar", true);
+                    animator.SetBool("Jugar2", false);
+                }
+                if(num>2f)
+                {
+                    animator.SetBool("Jugar", false);
+                    animator.SetBool("Jugar2", true);
+                }
                 break;
             case 5:
                 animator.SetBool("Dormir", false);
@@ -52,9 +92,12 @@ public class Animaciones : MonoBehaviour
     public void Reset()
     {
         animator.SetBool("Alimentar", false);
+        animator.SetBool("Alimentar2", false);
         animator.SetBool("Dormir", false);
         animator.SetBool("Jugar", false);
+        animator.SetBool("Jugar2", false);
         animator.SetBool("Bañar", false);
+        animator.SetBool("Bañar2", false);
     }
 
 }
